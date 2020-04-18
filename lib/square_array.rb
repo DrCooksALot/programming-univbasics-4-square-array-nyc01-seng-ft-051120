602c68a61fcbe8cefffab5401212b921c64294f0
Eli Nass <eliftw@gmail.com>
2020-04-18 08:57:14 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   new_array = []
   array.length.times  { | index |
-    new_array.push( array[index]**array[index] )
+    new_array.push( array[index]*array[index] )
   }
   new_array
 end
